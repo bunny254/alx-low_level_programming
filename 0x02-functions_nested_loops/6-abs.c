c43d6c226318a30bf033cc1a3c3adfca383fa2c7
bunny254 <kimaniwachira03@gmail.com>
2022-03-15 12:43:33 -0700

    Sixth abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - compute absolute value
+ * @n: integer
+ * Return: absolute value
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	return (0);
+}
