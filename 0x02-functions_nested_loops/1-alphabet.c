73507bfc2619e59ce89313f88a6d7f0baa172f2c
bunny254 <kimaniwachira03@gmail.com>
2022-03-15 11:32:10 -0700

    Second

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
