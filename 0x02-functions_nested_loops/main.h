b53829f9330404579bd5437668315c938a2a65bd
bunny254 <kimaniwachira03@gmail.com>
2022-03-15 11:21:50 -0700

    Another first

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,54 @@
+
+#ifndef MAIN_H_
+
+#define MAIN_H_
+
+
+
+int _putchar(char c);
+
+
+
+/* prototype that prints alphabet*/
+
+void print_alphabet(void);
+
+
+
+/*prototype - print 10 times the alphabet */
+
+void print_alphabet_x10(void);
+
+
+
+/*prototype - checks for lowercase char */
+
+int _islower(int c);
+
+
+
+/*prototype - checks for alphabetic char */
+
+int _isalpha(int c);
+
+
+
+/*prototype - print sign */
+
+int print_sign(int n);
+
+
+
+/* prototype - return absolute value */
+
+int _abs(int n);
+
+
+
+/* prototype - print last digit */
+
+int print_last_digit(int);
+
+
+
+#endif
