89376c9d072b2dd10c6099cabfdbf780f9b246e8
bunny254 <kimaniwachira03@gmail.com>
2022-03-15 11:12:18 -0700

    First

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - putchar
+ * _putcharprints the characters
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
