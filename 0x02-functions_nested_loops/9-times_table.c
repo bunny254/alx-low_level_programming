ffa9bea8d57145e6e16dce2a8e2169c45f93017c
bunny254 <kimaniwachira03@gmail.com>
2022-03-15 14:16:19 -0700

    Correction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,8 +18,7 @@ void times_table(void)
 			product = (row * column);
 																				if (column == 0)
 			{
-				_putchar('0' + product);
-																				}
+				_putchar('0' + product);													}
 			else if (product <= 9)
 			{
 				_putchar(',');																_putchar(' ');
